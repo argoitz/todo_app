b0d75f447ec68c7d5a9d0fb6c6ebe68ccd0afe59
Argoitz <argoitze@gmail.com>
2020-11-20 20:21:23 +0100

    on page or size change, fetch data from jsonplaceholder with limit and page values (If i implement this in the project, I have to call data first to set pages and every time I change this array (delete or add new task) to get the new array length), then I show in console this calls result (with pagination and limit)

--- src/custom-hooks/useTasks.js
+++ src/custom-hooks/useTasks.js
@@ -1,85 +1,86 @@
 import { useState, useEffect } from "react";
 
 const useTasks = () => {
-    console.log("Recorriendo script");
-    const [tasks, setTasks] = useState([]);
-    const getTasks = (tasks, page, pageSize) => tasks.slice(page*pageSize, (page + 1)*pageSize);
+  console.log("########### \n Runing Script \n########### \n");
+  const [tasks, setTasks] = useState([]);
 
-    const getDataFromUrl = async (limit = null, page = null) => {
-            // fetch('https://jsonplaceholder.typicode.com/todos')
-            // .then((res) => {
-            //     let responseJson = res.json().then((jRes) => {
-            //         console.log();
-            //         setTasks(jRes);
-            //     });
-            // })
-        if(limit) limit = "_limit="+limit;
-        if(page) page = "_page="+page;
-        if(limit && page) limit = limit+"&";
+  const getTasks = (tasks, page, pageSize) =>
+    tasks.slice(page * pageSize, (page + 1) * pageSize);
 
-        try {
-            console.log("GETTING DATA");
-            let url = 'https://jsonplaceholder.typicode.com/todos?'+limit+page;
-            console.log(url);
-            let response = await fetch(url);
-            let responseJson = await response.json();
-            setTasks(responseJson);
-           } catch(error) {
-            console.error(error);
-          }
+  const getDataFromUrl = async (page = null, limit = null) => {
+    if (limit) limit = "_limit=" + limit;
+    if (page) page = "_page=" + page;
+    if (limit && page) limit = limit + "&";
+
+    try {
+      let url = "https://jsonplaceholder.typicode.com/todos?" + limit + page;
+      console.log(url);
+      let response = await fetch(url);
+      return await response.json();
+    } catch (error) {
+      console.error(error);
     }
+  };
 
+  //Store searching word in localStorage
+  const useLocalStorageSearch = (defaultVal, key) => {
+    //check if localStorage key exist and get value or set default value
+    const [val, setVal] = useState(() => {
+      const localValue = window.localStorage.getItem(key);
+      return localValue !== null ? JSON.parse(localValue) : defaultVal;
+    });
+
+    //If new word is set, store in localStorage
     useEffect(() => {
-        setTimeout(() => {
-            getDataFromUrl(5,2);
-        }, 4000);
-    }, []);    
-        
-    //Store searching word in localStorage
-    const useLocalStorageSearch = (defaultVal, key) => {
-        //check if localStorage key exist and get value or set default value
-        const [val, setVal] = useState(() => {
-            const localValue = window.localStorage.getItem(key);
-            return localValue !== null? JSON.parse(localValue) : defaultVal
-        });
+      window.localStorage.setItem(key, JSON.stringify(val));
+    }, [key, val]);
 
-        //If new word is set, store in localStorage
-        useEffect(() => {
-            window.localStorage.setItem(key, JSON.stringify(val));
-        }, [key, val]);
+    return [val, setVal];
+  };
 
-        return [val, setVal];
-    };
-    
-    const [search, setSearch] = useLocalStorageSearch('', 'search');
+  const [search, setSearch] = useLocalStorageSearch("", "search");
 
+  const filteredTasks = tasks.filter((task) =>
+    task.title.toLowerCase().includes(search.toLowerCase())
+  );
+  const numOfTasks = filteredTasks.length;
+  const pageListSize = [5, 10, 15, 20, 25, 30];
+  const [page, setPage] = useState(0);
+  const [pageSize, setPageSize] = useState(5);
+  const pages = Math.ceil(numOfTasks / pageSize - 1);
 
-    const filteredTasks = tasks.filter(task => task.title.toLowerCase().includes(search.toLowerCase()));
-    const numOfTasks = filteredTasks.length;
-    const pageListSize = [5,10,15,20,25,30];
-    const [page, setPage] = useState(0);
-    const [pageSize, setPageSize] = useState(4);
-    const pages = Math.ceil((numOfTasks / pageSize) - 1);
+  useEffect(() => {
+    setTimeout(() => {
+      console.log("// - GET FULL DATA");
+      getDataFromUrl().then((res) => setTasks(res));
+    }, 4000);
+  }, []);
 
-    return {
-        tasks : {
-            numOfTasks,
-            fullTasks : tasks,
-            getTasks,
-            setTasks,
-            filteredTasks,
-            search,
-            setSearch,
-        },
-        pagination: {
-            page,
-            setPage,
-            pageSize,
-            setPageSize,
-            pages,
-            pageListSize
-        }
-    };
-}
+  useEffect(() => {
+    console.log("// - GET DATA ON CHANGE PAGE OR SIZE");
+    //Execute on change page, only for: (run on page change or size change test)
+    getDataFromUrl(page + 1, pageSize).then((res) => console.log(res));
+  }, [page, pageSize]);
+
+  return {
+    tasks: {
+      numOfTasks,
+      fullTasks: tasks,
+      getTasks,
+      setTasks,
+      filteredTasks,
+      search,
+      setSearch,
+    },
+    pagination: {
+      page,
+      setPage,
+      pageSize,
+      setPageSize,
+      pages,
+      pageListSize,
+    },
+  };
+};
 
 export default useTasks;
